5d470e42d5465415bc72c5f7a647ce46d138ace6
nosora0422 <142563767+nosora0422@users.noreply.github.com>
2025-03-20 18:00:20 -0700

    update

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,16 +1,45 @@
 import '../../index.css'
 
 import { Link, useLocation } from "react-router-dom";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import logo from '/assets/Codibara-Logo.png';
 
 
 export default function Header() {
     const location = useLocation();
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [hidden, setHidden] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > lastScrollY) {
+                setHidden(true); // Hide on scroll down
+            } else {
+                setHidden(false); // Show on scroll up
+            }
+            setLastScrollY(window.scrollY);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollY]);
+
+    useEffect(() => {
+        // When location changes, check for hash and scroll to the corresponding element
+        if (location.hash) {
+            const element = document.getElementById(location.hash.substring(1));
+            if (element) {
+                element.scrollIntoView({ behavior: "smooth", block: "start" });
+            }
+        }
+    }, [location]);
+
 
     return(
-        <div className="w-full max-w-[1440px] mx-auto sticky top-0 flex flex-col items-center py-3.5 px-6 md:flex-row md:justify-between z-50">
+        <div className={`w-full max-w-[1440px] mx-auto sticky top-0 flex flex-col items-center py-3.5 px-6 md:flex-row md:justify-between z-50 bg-white transition-transform duration-500 ${hidden ? "-translate-y-full" : "translate-y-0"}`}>
             <Link to='/'>
                 <img className='w-8' src={logo} />
             </Link>
@@ -20,9 +49,9 @@ export default function Header() {
                 </nav>
             ) : (
                 <nav className='flex justify-center gap-16 py-3'>
-                    <li className='nav-item'><a href="#about">About</a></li>
-                    <li className='nav-item'><a href="#works">Works</a></li>
-                    <li className='nav-item'><a href="#team">Team</a></li>
+                    <li className='nav-item'><Link to="#about">About</Link></li>
+                    <li className='nav-item'><Link to="#works">Works</Link></li>
+                    <li className='nav-item'><Link to="#team">Team</Link></li>
                 </nav>
             )}
         </div>
