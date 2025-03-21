5286f14aa9b96cfcaa89b50eebfcbabc23dba108
nosora0422 <142563767+nosora0422@users.noreply.github.com>
2025-03-20 18:10:30 -0700

    godaddy

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+     <Router> {/* Wrap the App component */}
+       <App />
+     </Router>
+   </StrictMode>
 )
--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,24 +1,12 @@
 import '../../index.css'
 
 import { Link, useLocation } from "react-router-dom";
-import { useEffect, useState } from 'react';
 
 import logo from '/assets/Codibara-Logo.png';
 
 
 export default function Header() {
     const location = useLocation();
-
-    useEffect(() => {
-        // When location changes, check for hash and scroll to the corresponding element
-        if (location.hash) {
-            const element = document.getElementById(location.hash.substring(1));
-            if (element) {
-                element.scrollIntoView({ behavior: "smooth", block: "start" });
-            }
-        }
-    }, [location]);
-
 
     return(
         <div className="w-full max-w-[1440px] mx-auto sticky top-0 flex flex-col items-center py-3.5 px-6 md:flex-row md:justify-between z-50">
@@ -31,9 +19,9 @@ export default function Header() {
                 </nav>
             ) : (
                 <nav className='flex justify-center gap-16 py-3'>
-                    <li className='nav-item'><Link to="#about">About</Link></li>
-                    <li className='nav-item'><Link to="#works">Works</Link></li>
-                    <li className='nav-item'><Link to="#team">Team</Link></li>
+                    <li className='nav-item'><a href="#about">About</a></li>
+                    <li className='nav-item'><a href="#works">Works</a></li>
+                    <li className='nav-item'><a href="#team">Team</a></li>
                 </nav>
             )}
         </div>
